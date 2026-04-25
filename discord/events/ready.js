afb98dfb15f3642cfb853cdad3e7bd4cbe5dd4fc
Conorjames98 <conor_1998@hotmail.co.uk>
2026-04-25 21:32:40 +0100

    Remove root discord folder, fix supabase env var in backend/discord

--- discord/events/ready.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(`✅ Logged in as ${client.user.tag}`)
-    client.user.setActivity('Recoil Customs', { type: 2 })
-  }
-}
